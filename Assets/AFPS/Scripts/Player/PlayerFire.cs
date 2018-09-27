8b71f57a37932146a82bdb2a6dda5e6e33d4e3a0
aiarehumantoo <juuso.t.hamalainen@gmail.com>
2018-09-27 19:03:29 +0300

    Beam SFX networking

--- Assets/AFPS/Scripts/Player/PlayerFire.cs
+++ Assets/AFPS/Scripts/Player/PlayerFire.cs
@@ -16,7 +16,8 @@ public class PlayerFire : NetworkBehaviour
     public bool rail;
     [SyncVar]
     public bool rl;
-    [SyncVar(hook = "OnWeaponChange")]
+    //[SyncVar(hook = "OnWeaponChange")]        //!!Hooked syncvar does not update automatically!!
+    [SyncVar]
     public string currentWeapon = "Gauntlet";
 
     // Stats
@@ -48,6 +49,13 @@ public class PlayerFire : NetworkBehaviour
     // SFX
     [SyncVar]
     bool useBeam;                                   // Does this weapon use beam sfx?
+    [SyncVar]
+    bool beamActive;
+    //[SyncVar]
+    Vector3 beamStartPos;
+    //[SyncVar]
+    Vector3 beamEndPos;
+
     float effectDisplayTime = 0.25f;                     // For how long beam is displayed
     //public ParticleSystem impactEffect;
     public AudioClip[] m_HitSounds;
@@ -57,6 +65,7 @@ public class PlayerFire : NetworkBehaviour
 
     Transform camera;                                       //camera location for shooting
 
+    [SyncVar]
     public float timer;                                    // A timer to determine when to fire.
     Ray shootRay = new Ray();                       // A ray from the gun end forwards.
     RaycastHit shootHit;                            // A raycast hit to get information about what was hit.
@@ -64,11 +73,18 @@ public class PlayerFire : NetworkBehaviour
 
     int numberOfPlayers;
 
+
     void Start()
     //public override void OnStartLocalPlayer()
     {
         //get camera
         camera = transform.GetChild(0);
+
+        // Get line renderer component
+        beamLine = GetComponent<LineRenderer>();
+
+        // Create a layer mask for the Shootable layer.
+        shootableMask = LayerMask.GetMask("Enemy", "Environment");
 
         if (!isLocalPlayer)
         {
@@ -81,14 +97,14 @@ public class PlayerFire : NetworkBehaviour
         gameObject.layer = 9;
 
         // Create a layer mask for the Shootable layer.
-        shootableMask = LayerMask.GetMask("Enemy", "Environment");
+        //shootableMask = LayerMask.GetMask("Enemy", "Environment");
 
         // Get audiosource
         m_AudioSource = GetComponent<AudioSource>();
 
         // Get line renderer component
-        beamLine = GetComponent<LineRenderer>();
-        beamLine.enabled = false;
+        //beamLine = GetComponent<LineRenderer>();                        // Cannot sync .enabled/.disabled between clients? Using [Syncvar] start/end pos instead (setting beam lenght to 0 when its disabled)
+        //beamLine.enabled = false;
 
         timer = timeBetweenShots; // Start without cooldown
 
@@ -96,11 +112,13 @@ public class PlayerFire : NetworkBehaviour
         CmdChangeWeapon("Gauntlet", 14, 0.055f, 2, false, false, null, 0, 0);
     }
 
+    /*
     // Called when string currentWeapon changes                         TESTING
     void OnWeaponChange(string currentWeapon)
     {
         //Debug.Log(currentWeapon);
     }
+    */
 
     /*
     void OnPlayerConnected(NetworkPlayer player)                                // Not called?
@@ -120,8 +138,7 @@ public class PlayerFire : NetworkBehaviour
             numberOfPlayers = NetworkManager.singleton.numPlayers;
             Debug.Log("Players; " +numberOfPlayers);
 
-            // Sync weapon
-            //CmdSyncWeapon(currentWeapon);                             weapon stats kept in sync with syncvar
+            // Sync weapon model
             CmdSyncWeaponModel(currentWeapon);
         }
         // dc
@@ -132,8 +149,52 @@ public class PlayerFire : NetworkBehaviour
         }
     }
 
+    // Get beam Start/End Pos.
+    void RenderBeam(bool enabled)                                                                //bool rapidFire        TODO: update positions once per shot for singleshot weapons (rail)
+    {
+        beamStartPos = transform.GetChild(0).GetChild(0).position;  // Weapon position
+
+        // Disabled
+        if (!enabled)
+        {
+            beamEndPos = transform.GetChild(0).GetChild(0).position;
+            RenderBeam();
+            return;
+        }
+
+        // Shoot ray forward from camera
+        shootRay.origin = camera.transform.position;
+        shootRay.direction = camera.transform.forward;
+
+        // Perform the raycast against gameobjects on the shootable layer and if it hits something...
+        if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
+        {
+            // Set the second position of the line renderer to the point the raycast hit.
+            beamEndPos = shootHit.point;
+        }
+        // If the raycast didn't hit anything on the shootable layer...
+        else
+        {
+            // ... set the second position of the line renderer to the fullest extent of the gun's range.
+            beamEndPos = shootRay.origin + shootRay.direction * range;
+        }
+
+        RenderBeam();
+    }
+
+    void RenderBeam() // (Vector3 startpos, vector3 endpos)
+    {
+        // Render beams locally for each player
+        beamLine.SetPosition(0, beamStartPos);
+        beamLine.SetPosition(1, beamEndPos);
+    }
+
+
     void Update()
     {
+        // Update beam SFX each frame
+        RenderBeam(beamActive);
+
         if (!isLocalPlayer)
         {
             return;
@@ -144,23 +205,6 @@ public class PlayerFire : NetworkBehaviour
 
         // Add the time since Update was last called to the timer.
         timer += Time.deltaTime;
-
-        // Update beam sfx starting position (weapon origin)
-        //if (useBeam && Input.GetButton("Fire1"))
-        if(useBeam && currentWeapon == "LG")
-        {
-            beamLine.SetPosition(0, transform.GetChild(0).GetChild(0).position);
-        }
-        //Disable beam sfx
-        //if (!Input.GetButton("Fire1") && timer >= effectDisplayTime)
-        if (currentWeapon == "Rail" && timer >= effectDisplayTime)
-        {
-            beamLine.enabled = false;
-        }
-        if (currentWeapon == "LG" && !Input.GetButton("Fire1"))
-        {
-            beamLine.enabled = false;
-        }
 
         // If the Fire1 button is being pressed and it's time to fire...
         if (Input.GetButton("Fire1") && timer >= timeBetweenShots)
@@ -175,8 +219,39 @@ public class PlayerFire : NetworkBehaviour
             }
             else
             {
-                CmdFireHitScan();
+                if (useBeam)
+                {
+                    CmdToggleBeam(true);
+                }
+
+                //FireHitScan();
             }
+        }
+
+        if (useBeam)
+        {
+            // Disable beam SFX
+            if (currentWeapon == "Rail" && timer >= effectDisplayTime)
+            {
+                CmdToggleBeam(false);
+            }
+            if (currentWeapon == "LG" && !Input.GetButton("Fire1"))
+            {
+                CmdToggleBeam(false);
+            }
+        }
+    }
+
+    [Command]
+    void CmdToggleBeam(bool enabled)
+    {
+        if (enabled)
+        {
+            beamActive = true;
+        }
+        else
+        {
+            beamActive = false;
         }
     }
 
@@ -188,9 +263,7 @@ public class PlayerFire : NetworkBehaviour
 
         // Create the projectile from the Prefab
         //var projectile = (GameObject)Instantiate(projectilePrefab, projectileSpawn, camera.rotation);                     //Cannot syncvar a gameobject
-        //var projectile = (GameObject)Instantiate(rocketPrefab, projectileSpawn, camera.rotation);
         GameObject projectile = null;
-
         if (currentWeapon == "RocketLauncher")
         {
             projectile = (GameObject)Instantiate(rocketPrefab, projectileSpawn, camera.rotation);
@@ -221,16 +294,8 @@ public class PlayerFire : NetworkBehaviour
         Destroy(projectile, projectileLifeTime);
     }
 
-    [Command]
-    void CmdFireHitScan()
-    {
-        // Enable line renderer
-        if (useBeam)
-        {
-            beamLine.SetPosition(0, transform.GetChild(0).GetChild(0).position);
-            beamLine.enabled = true;
-        }
-
+    void FireHitScan()                                                              //TODO damage networking
+    { 
         // Shoot ray forward from camera
         shootRay.origin = camera.transform.position;
         shootRay.direction = camera.transform.forward;
@@ -249,7 +314,8 @@ public class PlayerFire : NetworkBehaviour
                 knockback = camera.transform.forward.normalized * knockbackForce;
 
                 // ... the enemy should take damage.
-                playerHealth.TakeDamage(damagePerShot, knockback);
+                //playerHealth.TakeDamage(damagePerShot, knockback);
+                CmdDealDamage();
 
                 // Hp drops to 0 after taking damage
                 if (playerHealth.currentHealth <= 0)
@@ -263,29 +329,20 @@ public class PlayerFire : NetworkBehaviour
                 }
             }
 
-            // Set the second position of the line renderer to the point the raycast hit.
-            if (useBeam)
-            {
-                beamLine.SetPosition(1, shootHit.point);
-            }
-
             // Impact effect
             //Impact();
 
         }
-        // If the raycast didn't hit anything on the shootable layer...
-        else
-        {
-            // ... set the second position of the line renderer to the fullest extent of the gun's range.
-            if (useBeam)
-            {
-                beamLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
-            }
-        }
     }
 
-    // Woops. can "change" to already selected weapon. Will be problem later on with animations and whatnot         <-- FIX
+    [Command]
+    void CmdDealDamage()
+    {
 
+    }
+
+
+    // Woops. can "change" to already selected weapon. Will be problem later on with animations and whatnot         <-- FIX
     void SelectWeapon()
     {
         if (timer < timeBetweenShots || Input.GetButton("Fire1"))            // To change a weapon, current weapon must be ready to fire and player cannot be shooting
@@ -321,7 +378,7 @@ public class PlayerFire : NetworkBehaviour
     }
 
     /*
-    // Quick fix for syncing weapon states when new player joins                        not needed?
+    // Quick fix for syncing weapon states when new player joins
     [Command]
     void CmdSyncWeapon(string currentWeapon)
     {
---
+++ Assets/AFPS/Scripts/testing/playerfireold.cs
@@ -0,0 +1,472 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class playerfireold : NetworkBehaviour
+{
+    // Weapons, public for testing purposes
+    [SyncVar]
+    public bool gauntlet = true;
+    [SyncVar]
+    public bool plasma;
+    [SyncVar]
+    public bool lg;
+    [SyncVar]
+    public bool rail;
+    [SyncVar]
+    public bool rl;
+    [SyncVar(hook = "OnWeaponChange")]
+    public string currentWeapon = "Gauntlet";
+
+    // Stats
+    [SyncVar]
+    int damagePerShot = 14;                         // The damage inflicted by each shot.                        lg = 7, rail = 30, plasma = 20, rl = 100
+    [SyncVar]
+    float timeBetweenShots = 0.055f;              // The time between each shot.                          lg = 0.055f, rail = 1.5f, plasma = 0.11, rl  = 0.8
+    [SyncVar]
+    float range = 2;                             // The distance the gun can fire.
+    Vector3 knockback;
+    [SyncVar]
+    float knockbackForce = 5f;
+
+    // Projectiles
+    [SyncVar]
+    bool projectile = false;
+    GameObject projectilePrefab;             // Prefab of the projectile
+    float spawnDistance = 0.0f;                          // How far from the player projectile should spawn                         // Spawn distance is pointless with projectiles igonring the shooter. Might be needed for better visuals?
+    [SyncVar]
+    int projectileSpeed = 25;
+    float splashRadius;                                                                                                      // Currently using prefab size for explosion / splash size
+    [SyncVar]
+    int maximumSplashDamage;
+    float projectileLifeTime = 2.0f;                // For deleting projectiles that hit nothing
+
+    public GameObject rocketPrefab;     // Rocket projectile
+    public GameObject plasmaPrefab;     // Plasma projectile
+
+    // SFX
+    [SyncVar]
+    bool useBeam;                                   // Does this weapon use beam sfx?
+    float effectDisplayTime = 0.25f;                     // For how long beam is displayed
+    //public ParticleSystem impactEffect;
+    public AudioClip[] m_HitSounds;
+
+    private AudioSource m_AudioSource;
+    private LineRenderer beamLine;
+
+    Transform camera;                                       //camera location for shooting
+
+    public float timer;                                    // A timer to determine when to fire.
+    Ray shootRay = new Ray();                       // A ray from the gun end forwards.
+    RaycastHit shootHit;                            // A raycast hit to get information about what was hit.
+    int shootableMask;                              // A layer mask so the raycast only hits things on the shootable layer.
+
+    int numberOfPlayers;
+
+    void Start()
+    //public override void OnStartLocalPlayer()
+    {
+        //get camera
+        camera = transform.GetChild(0);
+
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        gameObject.name = "Local Player";
+
+        //Set layer of local player to "Player" (Layer 9) instead of "Enemy" layer
+        gameObject.layer = 9;
+
+        // Create a layer mask for the Shootable layer.
+        shootableMask = LayerMask.GetMask("Enemy", "Environment");
+
+        // Get audiosource
+        m_AudioSource = GetComponent<AudioSource>();
+
+        // Get line renderer component
+        beamLine = GetComponent<LineRenderer>();
+        beamLine.enabled = false;
+
+        timer = timeBetweenShots; // Start without cooldown
+
+        //Starting weapon (gauntlet) stats
+        CmdChangeWeapon("Gauntlet", 14, 0.055f, 2, false, false, null, 0, 0);
+    }
+
+    // Called when string currentWeapon changes                         TESTING
+    void OnWeaponChange(string currentWeapon)
+    {
+        //Debug.Log(currentWeapon);
+    }
+
+    /*
+    void OnPlayerConnected(NetworkPlayer player)                                // Not called?
+    {
+        Debug.Log("new player connected");
+        // Sync current state when new player joins
+        CmdSyncWeapon(currentWeapon);
+    }
+    */
+
+    void OnPlayerConnect()
+    {
+        // When new player joins
+        if (numberOfPlayers < NetworkManager.singleton.numPlayers)
+        {
+            numberOfPlayers = NetworkManager.singleton.numPlayers;
+            Debug.Log("Players; " + numberOfPlayers);
+
+            // Sync weapon
+            //CmdSyncWeapon(currentWeapon);                             weapon stats kept in sync with syncvar
+            CmdSyncWeaponModel(currentWeapon);
+        }
+        // dc
+        if (numberOfPlayers > NetworkManager.singleton.numPlayers)
+        {
+            numberOfPlayers = NetworkManager.singleton.numPlayers;
+            Debug.Log("Players; " + numberOfPlayers);
+        }
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        OnPlayerConnect();
+        SelectWeapon();
+
+        // Add the time since Update was last called to the timer.
+        timer += Time.deltaTime;
+
+        // Update beam sfx starting position (weapon origin)
+        //if (useBeam && Input.GetButton("Fire1"))
+        if (useBeam && currentWeapon == "LG")
+        {
+            beamLine.SetPosition(0, transform.GetChild(0).GetChild(0).position);
+        }
+        //Disable beam sfx
+        //if (!Input.GetButton("Fire1") && timer >= effectDisplayTime)
+        if (currentWeapon == "Rail" && timer >= effectDisplayTime)
+        {
+            beamLine.enabled = false;
+        }
+        if (currentWeapon == "LG" && !Input.GetButton("Fire1"))
+        {
+            beamLine.enabled = false;
+        }
+
+        // If the Fire1 button is being pressed and it's time to fire...
+        if (Input.GetButton("Fire1") && timer >= timeBetweenShots)
+        {
+            // Reset the timer.                                                                                         Note that resetting timer in [Command] would not reset timer for local player.
+            timer = 0f;
+
+            // ... shoot the gun.
+            if (projectile)
+            {
+                CmdFireProjectile();
+            }
+            else
+            {
+                CmdFireHitScan();
+            }
+        }
+    }
+
+    [Command]
+    void CmdFireProjectile()
+    {
+        // Spawn point
+        Vector3 projectileSpawn = camera.position + camera.transform.forward.normalized * spawnDistance;
+
+        // Create the projectile from the Prefab
+        //var projectile = (GameObject)Instantiate(projectilePrefab, projectileSpawn, camera.rotation);                     //Cannot syncvar a gameobject
+        //var projectile = (GameObject)Instantiate(rocketPrefab, projectileSpawn, camera.rotation);
+        GameObject projectile = null;
+
+        if (currentWeapon == "RocketLauncher")
+        {
+            projectile = (GameObject)Instantiate(rocketPrefab, projectileSpawn, camera.rotation);
+        }
+        if (currentWeapon == "Plasma")
+        {
+            projectile = (GameObject)Instantiate(plasmaPrefab, projectileSpawn, camera.rotation);
+        }
+
+        // Add velocity to the projectile
+        projectile.GetComponent<Rigidbody>().velocity = projectile.transform.forward * projectileSpeed;
+
+        // Other projectile stats
+        Projectile projectileScript = projectile.GetComponent<Projectile>();
+        projectileScript.damagePerShot = damagePerShot;
+        projectileScript.knockbackForce = knockbackForce;
+        projectileScript.splashDamage = maximumSplashDamage;
+
+        // Link this script
+        projectileScript.parentScript = GetComponent<PlayerFire>();
+        // Link player gameobject
+        projectileScript.parentGameObject = transform.gameObject;
+
+        // Spawn the projectile on the Clients
+        NetworkServer.Spawn(projectile);
+
+        // Destroy the projectile after x seconds
+        Destroy(projectile, projectileLifeTime);
+    }
+
+    [Command]
+    void CmdFireHitScan()
+    {
+        // Enable line renderer
+        if (useBeam)
+        {
+            beamLine.SetPosition(0, transform.GetChild(0).GetChild(0).position);
+            beamLine.enabled = true;
+        }
+
+        // Shoot ray forward from camera
+        shootRay.origin = camera.transform.position;
+        shootRay.direction = camera.transform.forward;
+
+        // Perform the raycast against gameobjects on the shootable layer and if it hits something...
+        if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
+        {
+
+            HitDummy();     // For testing
+
+            // If the Health component exists...
+            PlayerHealth playerHealth = shootHit.collider.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                //Calculate knockback
+                knockback = camera.transform.forward.normalized * knockbackForce;
+
+                // ... the enemy should take damage.
+                playerHealth.TakeDamage(damagePerShot, knockback);
+
+                // Hp drops to 0 after taking damage
+                if (playerHealth.currentHealth <= 0)
+                {
+                    // Last hit
+                    PlayHitSounds(true);
+                }
+                else
+                {
+                    PlayHitSounds(false);
+                }
+            }
+
+            // Set the second position of the line renderer to the point the raycast hit.
+            if (useBeam)
+            {
+                beamLine.SetPosition(1, shootHit.point);
+            }
+
+            // Impact effect
+            //Impact();
+
+        }
+        // If the raycast didn't hit anything on the shootable layer...
+        else
+        {
+            // ... set the second position of the line renderer to the fullest extent of the gun's range.
+            if (useBeam)
+            {
+                beamLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+            }
+        }
+    }
+
+    // Woops. can "change" to already selected weapon. Will be problem later on with animations and whatnot         <-- FIX
+
+    void SelectWeapon()
+    {
+        if (timer < timeBetweenShots || Input.GetButton("Fire1"))            // To change a weapon, current weapon must be ready to fire and player cannot be shooting
+        {
+            return;
+        }
+
+        // Input to select this weapon and player has it available
+        if (Input.GetButton("Gauntlet") && gauntlet)
+        {
+            CmdChangeWeapon("Gauntlet", 14, 0.055f, 2, false, false, null, 0, 0);
+        }
+
+        if (Input.GetButton("Plasma") && plasma)
+        {
+            CmdChangeWeapon("Plasma", 20, 0.11f, 0, false, true, plasmaPrefab, 25, 15);
+        }
+
+        if (Input.GetButton("RL") && rl)
+        {
+            CmdChangeWeapon("RocketLauncher", 100, 0.8f, 0, false, true, rocketPrefab, 25, 100);
+        }
+
+        if (Input.GetButton("LG") && lg)
+        {
+            CmdChangeWeapon("LG", 7, 0.055f, 50, true, false, null, 0, 0);
+        }
+
+        if (Input.GetButton("Rail") && rail)
+        {
+            CmdChangeWeapon("Rail", 90, 1.5f, 50, true, false, null, 0, 0);
+        }
+    }
+
+    /*
+    // Quick fix for syncing weapon states when new player joins                        not needed?
+    [Command]
+    void CmdSyncWeapon(string currentWeapon)
+    {
+        if(currentWeapon == "Gauntlet")
+            CmdChangeWeapon("Gauntlet", 14, 0.055f, 2, false, false, null, 0, 0);
+        if (currentWeapon == "Plasma")
+            CmdChangeWeapon("Plasma", 20, 0.11f, 0, false, true, plasmaPrefab, 25, 15);
+        if (currentWeapon == "RL")
+            CmdChangeWeapon("RocketLauncher", 100, 0.8f, 0, false, true, rocketPrefab, 25, 100);
+        if (currentWeapon == "LG")
+            CmdChangeWeapon("LG", 7, 0.055f, 50, true, false, null, 0, 0);
+        if (currentWeapon == "Rail")
+            CmdChangeWeapon("Rail", 90, 1.5f, 50, true, false, null, 0, 0);
+    }
+    */
+
+    [Command]
+    void CmdSyncWeaponModel(string weapon)
+    {
+        RpcChangeWeaponModel(weapon);
+    }
+
+    [Command]
+    void CmdChangeWeapon(string weapon, int damage, float firerate, float maxRange, bool beam, bool isProjectile, GameObject setProjectilePrefab, int setProjectileSpeed, int splashDmg)
+    {
+        currentWeapon = weapon;
+        useBeam = beam;
+        projectile = isProjectile;
+        damagePerShot = damage;
+        timeBetweenShots = firerate;
+        timer = firerate;
+        range = maxRange;
+        //knockbackForce        // same for all weapons. for now atleast
+
+        projectilePrefab = setProjectilePrefab;
+        projectileSpeed = setProjectileSpeed;
+        maximumSplashDamage = splashDmg;
+
+
+        // Show correct weapon model
+        RpcChangeWeaponModel(weapon);
+    }
+
+    [ClientRpc]
+    void RpcChangeWeaponModel(string weapon)
+    {
+        // Disable every weapon model
+        for (int i = 0; i < transform.GetChild(0).GetChild(0).childCount; i++)
+        {
+            transform.GetChild(0).GetChild(0).GetChild(i).gameObject.SetActive(false);
+        }
+
+        // Enable selected weapon model
+        transform.GetChild(0).GetChild(0).Find(weapon).gameObject.SetActive(true);
+
+    }
+
+    public void PlayHitSounds(bool kill)
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        if (kill) //killshot
+        {
+            m_AudioSource.clip = m_HitSounds[1];
+            m_AudioSource.PlayOneShot(m_AudioSource.clip);
+        }
+        else // normal hit
+        {
+            m_AudioSource.clip = m_HitSounds[0];
+            m_AudioSource.PlayOneShot(m_AudioSource.clip);
+        }
+    }
+
+    [Command]
+    public void CmdRespawn()
+    {
+        //Starting weapon (gauntlet) stats
+        CmdChangeWeapon("Gauntlet", 14, 0.055f, 2, false, false, null, 0, 0);
+
+        // Disable other weapons
+        plasma = false;
+        lg = false;
+        rl = false;
+        rail = false;
+    }
+
+    /*
+    private void Impact()
+    {
+        // effect location & rotation
+        impactEffect.transform.position = shootHit.point;
+        Vector3 dir = transform.position - shootHit.point;
+        impactEffect.transform.rotation = Quaternion.LookRotation(dir);
+        impactEffect.Play();
+    }
+    */
+
+    public void GiveWeapon(string weaponName)
+    {
+        if (weaponName == "Plasma")
+        {
+            plasma = true;
+        }
+        if (weaponName == "LG")
+        {
+            lg = true;
+        }
+        if (weaponName == "RL")
+        {
+            rl = true;
+        }
+        if (weaponName == "Rail")
+        {
+            rail = true;
+        }
+    }
+
+    void HitDummy()
+    {
+        // Try and find an Health script on the gameobject hit.
+        TargetDummy targetDummy = shootHit.collider.GetComponent<TargetDummy>();
+
+        if (targetDummy != null)
+        {
+            // Knockback calculated using position of the weapon, use camera location instead to get correct vector?
+            //knockback = transform.forward.normalized * knockbackForce;                                                    // Knockback direction (normalized) * force
+            //knockback = (shootHit.transform.position - transform.position).normalized * knockbackForce;                   // Same as before
+
+            knockback = camera.transform.forward.normalized * knockbackForce;
+
+            // ... the enemy should take damage.
+            targetDummy.TakeDamage(damagePerShot, knockback);
+
+            // Hp drops to 0 after taking damage
+            if (targetDummy.currentHealth <= 0)
+            {
+                PlayHitSounds(true);
+            }
+            else
+            {
+                PlayHitSounds(false);
+            }
+
+        }
+    }
+}
